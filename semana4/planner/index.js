47fd66de3e4d121d5f72a7119cf69cae9eae2808
maanoeln <mneto_004@hotmail.com>
2020-05-05 17:22:15 -0300

    alteraçoes

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -7,17 +7,15 @@ function insertTask(){
     if(task.length < 3){
         alert("Campo de tarefa em braco, por favor, digite algo!")
     }else{
-    document.getElementById(diaSemana).innerHTML += `<li onclick="taskDone()"> ${task} </li>`
+    document.getElementById(diaSemana).innerHTML += `<li class="done" onclick="taskDone()"> ${task} </li>`
     }
     document.getElementById("task").value = " "
 }
 
 function taskDone(){
-    const strikeWord = document.getElementsByTagName("li")
-
-    strikeWord.style = "text-decoration: line-through"
+    
 }
 
 function clearTask(){
-    document.getElementById("segunda").empty()
+    
 }
