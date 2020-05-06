b54b71fd641f92f1ed1706f19ee05e3d5e3dd72f
maanoeln <mneto_004@hotmail.com>
2020-05-06 17:48:51 -0300

    alteracoes

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,7 +1,8 @@
 let postInformation={
     title : null,
     author: null,
-    post : null
+    image: null,
+    post : null,
 }
 
 let postPublished = []
@@ -9,19 +10,32 @@ let postPublished = []
 function createPost() {
     let postTitle = document.getElementById("title")
     let postAuthor = document.getElementById("author")
+    let postImage = document.getElementById("image")
     let postContent = document.getElementById("post")
 
     let newPost={
         ...postInformation,
         title : postTitle.value,
         author : postAuthor.value,
+        image: postImage.value,
         post :  postContent.value,
     }
+    
+    postPublished.push(newPost)
 
-    postPublished.push(newPost)    
-    console.log(postPublished)
+    document.getElementById("postPublished").innerHTML += `Title: ${newPost.title}<br>Author: ${newPost.author}<br><img src="${newPost.image}"><br>Post: ${newPost.post}<br><br>`
+
+    postTitle.value = ""
+    postAuthor.value = ""
+    postImage.value = ""
+    postContent.value = ""  
 }
 
+function openPage(){
+    for(let i = 0; i < postPublished.length; i++){
+        document.getElementById("postPublished").innerHTML += `Title: ${newPost.title}<br>Author: ${newPost.author}<br><img src="${newPost.image}"><br>Post: ${newPost.post}<br><br>`
+    }
+}
 
 
 
