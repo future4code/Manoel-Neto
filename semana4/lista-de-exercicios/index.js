302db5240a90966f167d96c937c42b1e550a4ad3
maanoeln <mneto_004@hotmail.com>
2020-05-08 11:31:31 -0300

    exercicio interpretacao 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,6 @@
+Interpretacao de código
+
+ex.1 - existe uma funçao  que recebe como parametro valorEmDolar, que exibe um prompt o qual o usuario digita a cotacao do dolar.
+E retorna o valor convertido em real (valorEmDolar x cotacao). É criado uma variavel que receber essa funcao e o paramento para o 
+valorEmDolar de 100, ao rodar o usuario insere 5.70 como cotacao por exemplo, e a variavel recebe o valor de R$570.00 e é exibida
+no console.
