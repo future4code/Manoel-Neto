e8ac9dcbdf8ff138cc2b203d35a58873e7282b35
maanoeln <mneto_004@hotmail.com>
2020-05-10 15:19:50 -0300

    funcoes de array exercicio 5

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -336,3 +336,29 @@ function appointmentEmails(array){
 }
 
 appointmentEmails(appointments)
+
+// Ex. 5
+
+const accounts = [
+	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+]
+
+const newBalance = [400, 6260, -3340, -1900, 1300, 1200]
+
+function changeBalance(array){
+    accounts.forEach((account, index, array) => {
+        newBalance.forEach((balance, idx, arr) =>{
+            if(index === idx){
+            array[index].saldoTotal = arr[idx]
+        }
+        })
+    })
+    console.log(accounts)
+}
+
+changeBalance(accounts)
